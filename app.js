b2445283c30dcf31b3af776390e3b16abb40c090
Cristian Morales <cr15moresp@gmail.com>
2021-06-05 18:38:11 -0500

    Se creó una clase Server para manejar el servidor desde ese archivo

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 require('dotenv').config();
-const express = require('express')
-const app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen( process.env.PORT, () => {
-    console.log('Servidor corriendo en le puerto: ', process.env.PORT);
-});
 
+const Server = require('./models/server');
+
+
+const server = new Server
+
+server.listen();
 
