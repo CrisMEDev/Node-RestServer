60602ad86ea59495ae56bda4fb388eb9c1598dfe
Cristian Morales <cr15moresp@gmail.com>
2021-06-09 00:59:10 -0500

    Se retorno el numero total de registros en una colección y ejecutaron las promesas al mismo tiempo con promise.all

--- controllers/user.js
+++ controllers/user.js
@@ -7,14 +7,27 @@ const Usuario = require('../models/usuario');
 const usuariosGet = async(req = request, res = response) => {
 
     const { limite = 5, desde = 0 } = req.query;
+    const query = { estado: true };
 
     // TODO: Faltaría implementar una validación en caso de que el query param sea uns string y no un number
 
-    const usuarios = await Usuario.find()
+    // const usuarios = await Usuario.find(query)
+    //     .skip(Number(desde))
+    //     .limit(Number(limite));
+
+    // const total = await Usuario.countDocuments(query);
+
+    // Para ejecutar las promesas en simultaneo
+    const [ total, usuarios ] = await Promise.all([
+        Usuario.countDocuments(query),
+
+        Usuario.find(query)         // Solo devuelve el estado en true
         .skip(Number(desde))        // Registro en el que empieza
-        .limit(Number(limite));     // Cuantos registros tomará
+        .limit(Number(limite))      // Cuantos registros tomará
+    ]);
 
     res.json({
+        total,
         usuarios
     });
 }
