ae693c123a35a2661d7ce517799750c5dcd12404
Cristian Morales <cr15moresp@gmail.com>
2021-06-16 22:36:14 -0500

    Se agrego la opción que crea carpetas automaticamente para almacenar archivos cargados

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -9,9 +9,19 @@ const cargarArchivo = async( req = request, res = response ) => {
         return;
     }
 
-    const nombreImagen =  await subirArchivo( req.files );
+    try {
 
-    res.json({ nombreImagen });
+        // Mueve el archivo cargado a el directorio uploads/textos en la raiz del proyecto, si no existe el directorio, lo crea
+        // const nombreImagen =  await subirArchivo( req.files, [ 'txt', 'pdf' ], 'textos' );
+
+        // Mueve el archivo cargado a el directorio uploads en la raiz del proyecto
+        const nombreImagen =  await subirArchivo( req.files, undefined, 'imgs' );
+        res.json({ nombreImagen });
+    } catch (msg) {
+        res.status(400).json({ msg })
+    }
+
+
 
 }
 
